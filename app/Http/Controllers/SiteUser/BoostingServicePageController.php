e92c9c99e2cc5443b664738e29637c4c0a790a39
Wage Folabessy <mbappepsg336@gmail.com>
2025-04-01 15:56:35 +0700

    add subfolder

--- app/Http/Controllers/SiteUser/BoostingServicePageController.php
+++ app/Http/Controllers/SiteUser/BoostingServicePageController.php
@@ -13,13 +13,13 @@ class BoostingServicePageController extends Controller
     public function index()
     {
         $games = Game::has('boostingServices')->get();
-        return view('site-user.pages.joki-game', compact('games'));
+        return view('site-user.pages.joki-game.joki-game', compact('games'));
     }
 
     public function serviceSelection($gameId)
     {
         $game = Game::findOrFail($gameId);
-        return view('site-user.pages.pilih-layanan', compact('game'));
+        return view('site-user.pages.joki-game.pilih-layanan', compact('game'));
     }
 
     public function packageBoosting($gameId)
@@ -29,7 +29,7 @@ class BoostingServicePageController extends Controller
             ->where('service_type', 'package')
             ->get();
 
-        return view('site-user.pages.joki-paket', compact('game', 'packages'));
+        return view('site-user.pages.joki-game.joki-paket', compact('game', 'packages'));
     }
 
     public function customBoosting($gameId)
@@ -55,12 +55,12 @@ class BoostingServicePageController extends Controller
         $defaultTier = $defaultRank->rankTiers->sortBy('display_order')->first();
         $defaultTierDetail = $defaultTier ? $defaultTier->tierDetails->sortBy('display_order')->first() : null;
 
-        return view('site-user.pages.joki-kostum', compact('game', 'defaultRank', 'defaultTier', 'defaultTierDetail'));
+        return view('site-user.pages.joki-game.joki-kostum', compact('game', 'defaultRank', 'defaultTier', 'defaultTierDetail'));
     }
 
     public function show(BoostingService $service)
     {
         $service->load('game');
-        return view('site-user.pages.joki-paket-detail', compact('service'));
+        return view('site-user.pages.joki-game.joki-paket-detail', compact('service'));
     }
 }
